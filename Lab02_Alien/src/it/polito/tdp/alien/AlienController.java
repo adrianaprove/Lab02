7b9960094c2485b3b9346befbc13bfc1ba629bb9
Admin <Admin@DESKTOP-CDNDR7B>
2019-03-13 11:08:15 +0100

    Soluzione LAB2 con classe Word

--- Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab02_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -29,6 +29,8 @@ public class AlienController {
     private Button btnTranslate;
     @FXML
     private Button btnReset;
+    
+    AlienDictionary dizionario; 
         
     
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -38,18 +40,41 @@ public class AlienController {
     	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
     	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
     	
+    	dizionario=new AlienDictionary(); 
     }
   
     
     @FXML
     void doTranslate(ActionEvent event) {
+    	String s=txtWord.getText(); 
+    	System.out.println(s); 
+    	String[] info=s.split(" "); 
+    	if(info.length==2) {
+    		//STO INSERENDO UNA NUOVA PAROLA NEL DIZIONARIO
+    		String parola=info[0].toLowerCase(); 
+    		String traduzione=info[1].toLowerCase(); 
+    		if(parola.matches("[a-zA-Z]+") && traduzione.matches("[a-zA-Z]+")) {
+    			dizionario.addWord(parola, traduzione);
+    			//System.out.println("Salvata parola: "+parola+" con traduzione: "+traduzione+"\n"); 
+    			//txtResult.appendText("Salvata parola: "+parola+" con traduzione: "+traduzione+"\n");
+    	}
+    		else
+    			System.out.println("non matcha");
+    	}
+    	else if(info.length==1) {
+    		//STO CERCANDO LA TRADUZIONE DI QUELLA PAROLA
+    		String parola=info[0].toLowerCase(); 
+    		String traduzione=""; 
+    		if(parola.matches("[a-zA-Z]+"))
+    			traduzione=dizionario.translateWord(parola); 
+    		txtResult.appendText(traduzione+"\n");
+    	}
     	    	
     }
     
-    
     @FXML
     void doReset(ActionEvent event) {
-
+    	txtResult.clear();
     }
     
 }
